4ca42f34751c2c7873d270d8a10db4a81459719d
Jose Adrián <josealvarado@unicomfacauca.edu.co>
2022-06-22 13:57:16 -0500

    Ajustes App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+// import { BrowserRouter as Routes, Route, Router, Switch } from 'react-router-dom';
+
 import Inicio from './components/Inicio';
 import Personaje from './components/Personaje';
+import Validacion from './components/Validacion';
+import Visualizar from './components/Visualizar';
 import Vista from './components/Vista';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import './css/estilos.css';
+import Navigation from './components/Navigator';
 
 
 function App() {
   const [verPer, setVerPer] = useState(false);
-  const [verPdf, setVerPdf] = useState(false);
+  const [verValidacion, setVerValidacion] = useState(false);
 
-  const Menu = () =>{
+  const Menu = () => {
     return (
-    <nav className='nav'>
-      <button type="button" className="btn btn-primary"
-        onClick={()=>{
-          setVerPer(!verPer);        
-        }}
-      >
-      {verPer ? "Ocultar Lista" : "Ver lista"}</button>
-      <button type="button" className="btn btn-dark"
-        onClick={()=>{
-          setVerPdf(!verPdf);
-        }}
-      >{verPdf ? "Ocultar": " PDF"}</button>
-      
-      {/* <Button>Descargar PDF</Button> */}
-    </nav>
+      <nav>
+        <button type="button" className="btnVerPacientes btn btn-primary"
+          onClick={() => { setVerPer(!verPer); }}   >
+          {verPer ? "Ocultar Lista" : "Ver lista"}
+        </button>
+
+        <button type="button" className="btn btn-dark"
+          onClick={() => { setVerValidacion(!verValidacion); }}  >
+          {verValidacion ? "Ocultar" : " Validar"}
+        </button>
+      </nav>
     )
   };
+
   return (
-    <div className="container">
-      <br/>
-      <Menu/>
+    <div className='container p-4'>
+      <Menu />
       <BrowserRouter>
         <Routes>
-          {verPer ? <Route path='/'element ={<Inicio></Inicio>}></Route>: null}
-          <Route path='/persona/:id'element={<Personaje></Personaje>}></Route>
+          {verPer ? <Route path='/' element={<Inicio></Inicio>} /> : null}
+          <Route path='/persona/:id' element={<Personaje></Personaje>} />
+          <Route path='/visualizar' element={<Visualizar />} />
+          {verValidacion ? <Route path='/' element={<Validacion />} /> : null}
         </Routes>
       </BrowserRouter>
-      {verPdf ? <Vista/>: null}
     </div>
+
   );
 }
 
